333f646ecc7bc34b02cf863f9b30da08183ae80d
Daniel J. Finnegan <djf32@bath.ac.uk>
2019-03-05 14:32:36 +0000

    Created two helper batch scripts

---
+++ bibtex2csv.bat
@@ -0,0 +1,2 @@
+@rem Converts a bibtex file into a csv, dumping its keywords into output and creating a file of the same name
+@python bib2xyz.py --input-format=bibtex --output-format=csv --dump-keywords --output-dir=output --output-file=%~n1.csv %1
